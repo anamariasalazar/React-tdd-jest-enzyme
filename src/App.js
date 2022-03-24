e13ddaed765fbd6a6ea93d029478066f6725c8c1
ANA <ana.salazar@fadosolutions.com>
2022-03-24 16:34:09 -0500

    PWA

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-import './App.css';
-import Login from './Resorces/Login';
-import TaskPlanner from './Resorces/TaskPlanner';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import "./App.css";
+import Login from "./Resorces/Login";
+import TaskPlanner from "./Resorces/TaskPlanner";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-
-function App() { 
-    return (
-      <div>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/login" element={<Login/>}/>
-            <Route path='/TaskPlanner' element={<TaskPlanner/>}/>
-          </Routes>
-        </BrowserRouter>
-      </div>
-    )
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/TaskPlanner" element={<TaskPlanner />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
--- src/Resorces/TaskPlanner/index.js
+++ src/Resorces/TaskPlanner/index.js
@@ -1,37 +1,37 @@
-import React,{ useEffect, useState } from "react"
+import React, { useEffect, useState } from "react";
 
-const TaskPlanner=()=>{
-
-    const [tasks, setTasks] = useState("");
-    useEffect(()=>{
-        fetch("http://localhost:8080/v1/task",{
-            mode:'cors',
-            method:'GET',
-            headers:{
-                "Content-type": "application/json"
-            }
-        }).then(response => response.json())
-        .then(myJson =>{
-            setTasks(myJson);
-        })
-    },[])
-    return(
-        <div>
-            {
-                tasks && tasks.map((item,idx)=>{
-                    return <li key={idx}>
-                        <p>id: {item.id}</p>
-                        <p>name: {item.name}</p>
-                        <p>description: {item.description}</p>
-                        <p>status: {item.status}</p>
-                        <p>assignedTo: {item.assignedTo}</p>
-                        <p>dueDate: {item.dueDate}</p>
-                        <p>created: {item.created}</p>
-                    </li>
-                })
-                
-            }
-        </div>
-    )
-}
-export default TaskPlanner
+const TaskPlanner = () => {
+  const [tasks, setTasks] = useState("");
+  useEffect(() => {
+    fetch("http://localhost:8080/v1/task", {
+      mode: "cors",
+      method: "GET",
+      headers: {
+        "Content-type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((myJson) => {
+        setTasks(myJson);
+      });
+  }, []);
+  return (
+    <div>
+      {tasks &&
+        tasks.map((item, idx) => {
+          return (
+            <li key={idx}>
+              <p>id: {item.id}</p>
+              <p>name: {item.name}</p>
+              <p>description: {item.description}</p>
+              <p>status: {item.status}</p>
+              <p>assignedTo: {item.assignedTo}</p>
+              <p>dueDate: {item.dueDate}</p>
+              <p>created: {item.created}</p>
+            </li>
+          );
+        })}
+    </div>
+  );
+};
+export default TaskPlanner;
